13cb19c4fcc23e64f6c86fb7644741ba558d2d56
凝萌QAQ <958308968@qq.com>
2026-05-05 23:36:56 +0800

    refactor: 删除旧版主题文件及其相关资源
    清理不再使用的GodMainCode主题文件，包括模板文件、静态资源和数据库脚本

--- wp-content/themes/GodMainCode/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text">搜索：</span>
-		<input type="search" class="search-field" placeholder="搜索..." value="<?php echo get_search_query(); ?>" name="s">
-	</label>
-	<button type="submit" class="search-submit">搜索</button>
-</form>
